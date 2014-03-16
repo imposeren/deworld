5e518c822593d61ad41ebc42cc47359c16a2f978
Yaroslav Klyuyev <imposeren@gmail.com>
2014-03-16 17:18:48 +0200

    colormap for terrain

--- deworld/visualise/cmaps.py
+++ deworld/visualise/cmaps.py
@@ -3,8 +3,10 @@ import matplotlib
 from matplotlib.colors import colorConverter
 
 # generate the transparent colors
-cyan_transp = colorConverter.to_rgba('cyan', alpha=0.4)
-red_transp = colorConverter.to_rgba('red', alpha=0.4)
+terrain_bot = colorConverter.to_rgba('#71abd8', alpha=0.7)
+terrain_mid = colorConverter.to_rgba('#acd0a5', alpha=0.7)
+terrain_high = colorConverter.to_rgba('#c3a76b', alpha=0.7)
+terrain_top = colorConverter.to_rgba('#f5f4f2', alpha=0.7)
 
 # make the colormaps
-red_cyan_transp = matplotlib.colors.LinearSegmentedColormap.from_list('my_cmap2', [cyan_transp, red_transp], 256)
+red_cyan_transp = matplotlib.colors.LinearSegmentedColormap.from_list('my_cmap2', [terrain_bot, terrain_mid, terrain_high, terrain_top], 256)
