f21a1c6bd01b7be7302cb21f0543ab6e9c7429d7
Yaroslav Klyuyev <imposeren@gmail.com>
2014-03-16 10:48:09 +0200

    map drawing code is a separate widget class now

--- deworld/visualise/base.py
+++ deworld/visualise/base.py
@@ -2,6 +2,7 @@
 
 # system:
 import sys
+import random
 
 # thirdparty:
 from PySide import QtGui
@@ -12,30 +13,14 @@ from deworld.configs import BaseConfig
 from deworld.map_colors import HeightColorMap
 
 
-class Visualiser(QtGui.QWidget):
-
-    def __init__(self):
-        super(Visualiser, self).__init__()
-        self.world = World(25, 25, BaseConfig)
-        self.world.do_step()
+class MapDrawer(QtGui.QWidget):
+    def __init__(self, world):
+        super(MapDrawer, self).__init__()
+        self.world = world
         self.cell_size = (10, 10)
 
-        self.initUI()
-
-    @property
-    def map_size(self):
-        return self.cell_size[0] * self.world.w, self.cell_size[1] * self.world.h
-
-    def initUI(self):
-        self.setGeometry(300, 300, *self.map_size)
-        self.setWindowTitle('DeWorld Visualiser')
-        self.show()
-
-    def paintEvent(self, e):
-        qp = QtGui.QPainter()
-        qp.begin(self)
-        self.draw_map(qp)
-        qp.end()
+        w, h =self.map_size
+        self.setMinimumSize(w+1, h+1)
 
     def draw_map(self, qp):
         layer = self.world.layer_height
@@ -50,6 +35,49 @@ class Visualiser(QtGui.QWidget):
                 x += self.cell_size[0]
             y += self.cell_size[1]
             x = 0
+
+    def paintEvent(self, e):
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        self.draw_map(qp)
+        qp.end()
+
+    @property
+    def map_size(self):
+        return self.cell_size[0] * self.world.w, self.cell_size[1] * self.world.h
+
+
+class Visualiser(QtGui.QWidget):
+
+    def __init__(self, world=None):
+        super(Visualiser, self).__init__()
+        if world is None:
+            self.world = World(25, 25, BaseConfig)
+            self.randomize_world()
+        else:
+            self.world = world
+
+        self.initUI()
+
+    def randomize_world(self):
+        for x in range(self.world.w):
+            for y in range(self.world.h):
+                self.world.layer_height.data[y][x] = random.random()
+
+    def initUI(self):
+        self.map_widget = MapDrawer(self.world)
+
+        grid = QtGui.QGridLayout()
+
+        grid.addWidget(self.map_widget, 1, 0)
+        grid.addWidget(QtGui.QLabel('Test'), 1, 1)
+
+        self.setLayout(grid)
+        w, h = self.map_widget.map_size
+        #self.setGeometry(300, 300, w+200, h)
+        self.setWindowTitle('DeWorld Visualiser')
+        self.show()
+
 
 
 def main():
